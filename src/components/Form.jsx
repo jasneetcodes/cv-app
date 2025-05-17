2ae8270fbfe83ebedff71811922538061a386836
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-05-17 17:45:01 -0600

    feat: add state to general info component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,11 +2,25 @@ import EducationInfo from "./EducationInfo";
 import GeneralInfo from "./GeneralInfo";
 import "../styles/formStyles.css"
 import ExperienceInfo from "./ExperienceInfo";
+import { useState } from "react";
 function Form() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+
+  function handleName(e) {
+    setName(e.target.value);
+  }
+  function handleEmail(e) {
+    setEmail(e.target.value);
+  }
+  function handleNumber(e) {
+    setNumber(e.target.value);
+  }
   return (
     <div className="form-container">
       <h1>Resume Builder</h1>
-      <GeneralInfo />
+      <GeneralInfo name={name} email={email} number={number} nameHandle={handleName} emailHandle={handleEmail} numberHandle={handleNumber}/>
       <EducationInfo />
       <ExperienceInfo/>
       <button>Create Resume</button>
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,18 +1,18 @@
 
 
-function GeneralInfo(){
+function GeneralInfo({nameHandle, numberHandle, emailHandle, name, email, number}){
 
     return(
         <>
         <h2>General Info</h2>
         <label htmlFor="name">Name</label>
-        <input id="name" ></input>
+        <input id="name" value={name} onChange={nameHandle}></input>
         
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" ></input>
+        <input type="email" id="email" value={email} onChange={emailHandle} ></input>
 
         <label htmlFor="number">Number</label>
-        <input type="number" id="number" ></input>
+        <input type="tel" id="number" value={number} onChange={numberHandle}></input>
         </>
     )
 }
