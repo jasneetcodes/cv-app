e15294a7a03c41835d61a0ded070dbe8c77ec137
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-06-01 16:50:52 -0600

    feat: create states and props to change educational info

--- src/components/EducationInfo.jsx
+++ src/components/EducationInfo.jsx
@@ -1,18 +1,18 @@
 
 
-function EducationInfo(){
+function EducationInfo({school, title, time, schoolHandle, titleHandle, timeHandle}){
 
     return(
         <>
         <h2>Education Info</h2>
         <label htmlFor="studySchool">School</label>
-        <input type="text" id="studySchool" />
+        <input type="text" id="studySchool" value={school} onChange={schoolHandle} />
 
         <label htmlFor="studyTitle">Education Title</label>
-        <input type="text" id="studyTitle" />
+        <input type="text" id="studyTitle" value={title} onChange={titleHandle}/>
 
         <label htmlFor="studyDate">Graduation Date</label>
-        <input type="number" id="studyDate" />
+        <input type="number" id="studyDate" value={time} onChange={timeHandle}/>
         </>
     )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,10 +4,15 @@ import "../styles/formStyles.css"
 import ExperienceInfo from "./ExperienceInfo";
 import { useState } from "react";
 function Form() {
+  const [active, setActive] = useState(0);
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [number, setNumber] = useState('');
-  const [active, setActive] = useState(0);
+  
+  const [school, setSchool] = useState('');
+  const [studyTitle, setStudyTitle] = useState('');
+  const [studyTime, setStudyTime] = useState('');
 
   function handleName(e) {
     setName(e.target.value);
@@ -27,14 +32,25 @@ function Form() {
       <div className="form-container">
       <h1>Resume Builder</h1>
       <GeneralInfo name={name} email={email} number={number} nameHandle={handleName} emailHandle={handleEmail} numberHandle={handleNumber}/>
- 
+      <EducationInfo 
+      school={school} 
+      title={studyTitle} 
+      time={studyTime}
+      schoolHandle = {(e) => setSchool(e.target.value)}
+      titleHandle = {(e) => setStudyTitle(e.target.value)}
+      timeHandle = {(e) => setStudyTime(e.target.value)}
+      />
       <button onClick={() => setActive(1)}>Create Resume</button>
     </div>
     ) : (
       <div className="form-container">
-      <h1>{name}</h1>
-      
- 
+      <h1>Name:{name}</h1>
+      <h1>Email:{email}</h1>
+      <h1>Number:{number}</h1>
+      <hr />
+      <h1>school:{school}</h1>
+      <h1>studyTitle:{studyTitle}</h1>
+      <h1>studyTime:{studyTime}</h1>
       <button onClick={() => setActive(0)}>Edit Resume</button>
     </div>
     )}
